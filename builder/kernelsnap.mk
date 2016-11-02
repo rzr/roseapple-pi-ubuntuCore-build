a611dbaf66254ef6977f0bec49be4cd5d02225aa
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-11-02 11:41:30 +0000

    Revert "Adding workaround till snapcraft supports kernel.img in kernel snap"
    This reverts commit aac6b431ea3c1bf57c955de45ee24279c771aea6.

--- builder/kernelsnap.mk
+++ builder/kernelsnap.mk
@@ -11,14 +11,10 @@ clean:
 
 distclean: clean
 	rm -rf $(wildcard $(KERNEL_SRC))
-
+	
 build:
 	if [ ! -d $(KERNEL_SRC) ] ; then git clone $(KERNEL_REPO) -b $(KERNEL_BRANCH) kernel; fi
 	cd $(KERNEL_SRC); snapcraft clean; snapcraft --target-arch armhf snap
-	sudo rm -rf $(OUTPUT_DIR)/kernel-snap
-	mkdir $(OUTPUT_DIR)/kernel-snap
-	sudo unsquashfs -f -d $(OUTPUT_DIR)/kernel-snap $(KERNEL_SRC)/$(KERNEL_SNAP)
-	sudo sudo ln -P $(OUTPUT_DIR)/kernel-snap/vmlinuz $(OUTPUT_DIR)/kernel-snap/kernel.img
-	sudo mksquashfs $(OUTPUT_DIR)/kernel-snap/ $(OUTPUT_DIR)/$(KERNEL_SNAP)
-
+	cp $(KERNEL_SRC)/$(KERNEL_SNAP) $(OUTPUT_DIR)
+	
 .PHONY: build
