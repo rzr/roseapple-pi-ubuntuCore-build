0e914a7cd2df05d899e8a933532161e03170b23d
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-11-03 13:11:35 +0000

    Renaming uboot variable in gadget.mk for consistency
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- builder/gadget.mk
+++ builder/gadget.mk
@@ -1,6 +1,6 @@
 include common.mk
 
-OEM_UBOOT_BIN := $(GADGET_DIR)/boot-assets/u-boot.bin
+GADGET_UBOOT_BIN := $(GADGET_DIR)/boot-assets/u-boot.bin
 GADGET_SNAP := $(OUTPUT_DIR)/roseapple-pi_$(GADGET_VERSION)*.snap
 
 # for preloader packaging
@@ -23,7 +23,7 @@ distclean: clean
 u-boot:
 	@if [ ! -d $(GADGET_DIR)/boot-assets ] ; then mkdir $(GADGET_DIR)/boot-assets; fi
 	@if [ ! -f $(UBOOT_BIN) ] ; then echo "Build u-boot first."; exit 1; fi
-	cp -f $(UBOOT_BIN) $(OEM_UBOOT_BIN)
+	cp -f $(UBOOT_BIN) $(GADGET_UBOOT_BIN)
 
 preload: u-boot
 	$(TOOLS_DIR)/utils/$(BOOTLOADER_PACK) $(PRELOAD_DIR)/bootloader.bin $(PRELOAD_DIR)/bootloader.ini $(GADGET_DIR)/boot-assets/bootloader.bin
