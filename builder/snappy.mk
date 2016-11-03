51184f6b18f2154690721929f5318c8f58f31230
Ondrej Kubik <ondrej.kubik@canonical.com>
2016-11-03 13:42:07 +0000

    Remove all core things from snappy.mk
    Signed-off-by: Ondrej Kubik <ondrej.kubik@canonical.com>

--- builder/snappy.mk
+++ builder/snappy.mk
@@ -2,8 +2,6 @@ include common.mk
 
 SNAPPY_IMAGE := $(shell i="0"; while ls roseapple-pi-`date +%Y%m%d`-$${i}.img* 1> /dev/null 2>&1; do i=$$((i+1)); done; echo "roseapple-pi-`date +%Y%m%d`-$${i}.img")
 # yes for latest version; no for the specific revision of edge/stable channel
-SNAPPY_CORE_NEW := yes
-SNAPPY_CORE_VER ?=
 SNAPPY_CORE_CH := edge
 GADGET_SNAP := roseapple-pi_$(GADGET_VERSION)_armhf.snap
 KERNEL_SNAP_VERSION := `cat $(KERNEL_SRC)/prime/meta/snap.yaml | grep version: | awk '{print $$2}'`
@@ -18,9 +16,6 @@ clean:
 distclean: clean
 
 build-snappy:
-ifeq ($(SNAPPY_CORE_NEW),no)
-		$(eval REVISION = --revision $(SNAPPY_CORE_VER))
-endif
 	@echo "build snappy..."
 	sudo UBUNTU_DEVICE_FLASH_IGNORE_UNSTABLE_GADGET_DEFINITION=y $(UDF) core 16 -v \
 		--channel $(SNAPPY_CORE_CH) \
